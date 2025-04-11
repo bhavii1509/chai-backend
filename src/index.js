12c6ad38c946ca8141ce91c8c990d75d130db9cc
bhavii1509 <bhavishya1509@gmail.com>
2025-04-11 07:51:09 -0700

    utils configurations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
+
+dotenv.config({
+    path: "./.env"
+})
+
 connectDB()
+.then(() => {
+    app.on("error",(error) => {
+        console.log("Error occur at runtime while the server is running ",error)
+        throw error;
+    })
+    app.listen((process.env.PORT || 8000), ()=>{
+        console.log(`Server is running at port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err) => {
+    console.log("ERROR",err)
+})
 
 
 
